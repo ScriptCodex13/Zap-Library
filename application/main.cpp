ffff6b4961b1a5321fa79f3e7e56846adb6615de
ScriptCodex13 <lukbauhome@gmail.com>
2025-08-14 20:15:05 +0200

    @armagedescu Changed it to match your code

---
+++ application/main.cpp
@@ -0,0 +1,116 @@
+// Just a example
+
+#include "Zap.h" // Please use intern Header with ""
+#include "Mesh.h"
+#include <iostream>
+#include <vector>
+
+
+const char* vertexShaderSource = R"glsl(#version 330 core
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
+layout(location = 2) in vec2 aTexCoord;
+
+out vec3 ourColor;
+out vec2 TexCoord;
+
+void main()
+{
+	gl_Position = vec4(aPos, 1.0);
+	ourColor = aColor;
+	TexCoord = aTexCoord;
+} )glsl";
+
+const char* fragmentShaderSource = R"glsl(#version 330 core
+out vec4 FragColor;
+  
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
+
+void main()
+{
+    FragColor = texture(ourTexture, TexCoord);
+})glsl";
+
+
+int main()
+{
+	zap::Init();
+
+	zap::Window window(1920, 1080, "Hello Window");
+
+	zap::InitGlad();
+
+	//window.SetFullscreen(true);
+	//window.UpdateViewport();
+
+	std::vector<float> vertices =
+	{
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right 
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right  This causes the bug
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left		And also this
+
+	};
+
+	std::vector<unsigned int> indices =
+	{
+		0, 1, 3,
+		1, 2, 3
+	};
+
+
+	//Mesh
+
+	zap::Mesh2D mesh(&vertices, &indices);
+
+	mesh.SetVertexShaderSource(vertexShaderSource);
+	mesh.SetFragmentShaderSource(fragmentShaderSource);
+
+	mesh.SetAttribPointer(0, 3, 8, 0);
+	mesh.SetAttribPointer(1, 3, 8, 3);
+	mesh.SetAttribPointer(2, 2, 8, 6);
+
+	unsigned int texture0Id = mesh.InitTexture(0, "C:/Dev/Zap/Rec/Textures/texture.jpg", 0, zap::TextureFilters::LINEAR, zap::MipmapSettings::LINEAR_MIPMAP_LINEAR, zap::TextureWrapping::CLAMP_TO_EDGE)->i_id;
+
+	mesh.Finish();
+
+
+	window.UpdateViewport(); //This is a set callback. Once set == forever set
+
+	while (window.Open())
+	{
+		//window.StartTime();
+
+
+		if (window.isKeyPressed(zap::Key::ESC))
+		{
+			window.Close();
+		}
+
+		//from here draw starts
+		//there starts general draw
+		window.ShowWireFrame(window.isKeyPressed(zap::Key::F10));
+		window.ClearBackground(zap::BackgroundColors::BLACK);
+		//here starts current VAO for current program draw
+		mesh.bind(); //set current context before any draw routines, it prevents mess in more complex programs
+		mesh.SetTexture(texture0Id); //return false if texture not found
+		mesh.Write();
+		//here draw ends
+
+		window.SetTitle(std::to_string(window.GetDelta()));
+
+		window.Update();
+		window.Draw();
+
+		//std::cout << window.GetFPS() << std::endl;
+	}
+
+	zap::Delete();
+
+}
+
+
