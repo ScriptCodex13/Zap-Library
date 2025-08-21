2bee978b1a5a2853efdd9ee2f89d348fbd95534a
Ion Filipski <armagedescu@gmail.com>
2025-08-21 18:38:11 +0300

    Add lib extractor, 7zip based

---
+++ 3rdparty/extract_libs.cmd
@@ -0,0 +1,14 @@
+@del .\ChromeRoatta.zip /Q
+@set zip7zPath="%ProgramFiles%\7-Zip\7z.exe"
+@if not exist %zip7zPath% set zip7zPath="%ProgramFiles(x86)%\7-Zip\7z.exe"
+@if not exist %zip7zPath% set zip7zPath="7z.exe"
+@if not exist %zip7zPath% goto end
+rd /s /q glfw-3.4.bin.WIN64 glm-0.9.9.7 stb
+@rem goto finish
+@%zip7zPath% x .\glfw-3.4.bin.WIN64.zip
+@%zip7zPath% x .\glm-0.9.9.7.zip -oglm-0.9.9.7
+@%zip7zPath% x .\stb.zip -ostb
+@goto finish
+:end
+@echo unsuccessfull finish, probably 7z not installed
+:finish
