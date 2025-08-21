6f55e6013de5f81ca2d2871ce7c0c899173abe30
Ion Filipski <armagedescu@gmail.com>
2025-08-21 18:50:15 +0300

    Add 7z command line tool locally

--- 3rdparty/extract_libs.cmd
+++ 3rdparty/extract_libs.cmd
@@ -1,5 +1,6 @@
 @del .\ChromeRoatta.zip /Q
-@set zip7zPath="%ProgramFiles%\7-Zip\7z.exe"
+@set zip7zPath=".\7z.exe"
+@if not exist %zip7zPath% set zip7zPath="%ProgramFiles%\7-Zip\7z.exe"
 @if not exist %zip7zPath% set zip7zPath="%ProgramFiles(x86)%\7-Zip\7z.exe"
 @if not exist %zip7zPath% set zip7zPath="7z.exe"
 @if not exist %zip7zPath% goto end
