65b85977f4423229aed553a5b4ff8a48fbd74e95
ScriptCodex13 <lukbauhome@gmail.com>
2025-08-26 16:26:20 +0200

    Update Resources.h

--- src/Resources.h
+++ src/Resources.h
@@ -1,1 +1,17 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+namespace zap
+{
+	/*****************************************************************************/
+	
+	//APIs Resources
+
+	void Init();       // Initialize GLFW 
+	void InitGlad();   // Initialize glad
+	void Delete();     // Delete GLFW resources
+
+	/*****************************************************************************/
+
+}
 
