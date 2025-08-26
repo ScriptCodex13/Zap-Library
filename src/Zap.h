181e8633d829a8ad751a95ae5f8c928e0f4ecbd4
ScriptCodex13 <lukbauhome@gmail.com>
2025-08-26 16:28:54 +0200

    Did the TODO from Zap.h

--- src/Zap.h
+++ src/Zap.h
@@ -2,11 +2,7 @@
 
 #ifndef ZAP_H
 #define ZAP_H
-//TODO: Is zap.h intendet to gather headers or to add functionality?
-//      It must be either only the one, or only the second one, not both at the same time.
-//      The "std" prefix is widely used for headers containing only #includes
-//      Widespread examples of such names as StdAfx.h, stdc++.h
-//      Consider adopting this practice
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -26,6 +22,7 @@
 //
 
 #endif
+
 
 
 
