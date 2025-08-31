402bcffaedf1d31de57cedaf04e4c176975ee21b
ScriptCodex13 <lukbauhome@gmail.com>
2025-08-31 12:11:55 +0200

    Move texture folder to Project now

--- Build_Premake/Windows_GenerateVS.bat
+++ Build_Premake/Windows_GenerateVS.bat
@@ -4,6 +4,8 @@ setlocal ENABLEDELAYEDEXPANSION
 @REM mkdir "..\Project"
 mkdir Project
 premake5.exe vs2022
+move "../Zap3DDemo/Zap3DTrianglesBasic/textures" "./Project/Application"
 endlocal
 PAUSE
+
 
