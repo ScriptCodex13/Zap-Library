67b8e9d72fa2b5e4d2be0d7587eb66c2885fdbd9
Not Important <lukbauhome@gmail.com>
2025-09-02 14:01:41 +0200

    Added new rewrite files

---
+++ src/core/zap_core.cpp
@@ -0,0 +1,34 @@
+#include "zap_core.h"
+
+// Windows 32 or 64 bit
+
+#ifdef __WIN32
+
+
+
+#endif
+
+
+// Apple Desktop
+
+#ifdef __APPLE__
+#ifdef TARGET_OS_MAC
+
+
+
+
+#endif
+
+#endif
+
+
+// Linux 
+
+#ifdef __linux__
+
+
+
+#endif
+
+
+
