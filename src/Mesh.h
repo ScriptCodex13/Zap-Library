0c7cd5ecd85f6fb563666424f4cbcfe3767be406
Not Important <lukbauhome@gmail.com>
2025-09-10 17:31:20 +0200

    Use <> to indicate that glm libraries are extern

--- src/Mesh.h
+++ src/Mesh.h
@@ -10,9 +10,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/type_ptr.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 
 #include <vector>
