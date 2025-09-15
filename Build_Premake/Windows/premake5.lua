111d18801936239a99c2a71099d1ca2196df9255
Not Important <lukbauhome@gmail.com>
2025-09-15 20:31:30 +0200

    Moved Windows Build System into a seperate folder

---
+++ Build_Premake/Windows/premake5.lua
@@ -0,0 +1,75 @@
+workspace "Zap-Project"
+    architecture "x64"
+
+    location "Project"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+project "Zap"
+    location "Project/Zap"
+    kind "StaticLib"
+    language "C++"
+    files { "../../src/**.cpp"}
+    files { "../../src/**.h"}
+    files { "../../src/Graphics/**.cpp"}
+    files { "../../src/Graphics/**.h"}
+    files { "../../src/GUI**.cpp"}
+    files { "../../src/GUI/**.h"}
+    files { "../../src/Util/**.cpp"}
+    files { "../../src/Util/**.h"}
+    files { "../../src/Window/**.cpp"}
+    files { "../../src/Window/**.h"}
+    includedirs {"../../extern/"}
+    files {"../../extern/stb_image.h"}
+    includedirs {"../../extern/DEPENDENCIES (glad)(GLFW)/include/"}
+    includedirs {"../../extern/freetype/include"}
+    cppdialect "C++17"
+
+    filter "configurations:Debug"
+        staticruntime "off"
+        runtime "Debug"
+
+
+project "Application"
+    location "Project/Application"
+    kind "ConsoleApp"
+    language "C++"
+    files {"../../application/main.cpp"}
+    files {"../../extern/glad.c"}
+    includedirs {"../../extern/DEPENDENCIES (glad)(GLFW)/include/"}
+    includedirs {"../../extern/freetype/include"}
+    includedirs {"../../src/"}
+    includedirs {"../../extern/"}
+    libdirs {"../../extern/DEPENDENCIES (glad)(GLFW)/lib/"}
+    libdirs {"../../extern/freetype/lib"}
+    libdirs {"../../Project/Zap/bin/"}
+    links {"Zap"}
+
+    cppdialect "C++17"
+
+    filter "configurations:Debug"
+        staticruntime "off"
+        runtime "Debug"
+    
+    filter "system:windows"
+        links {"glfw3","opengl32","user32", "gdi32","shell32", "freetype"} -- Add other platforms support with freetype later
+    
+    filter "system:linux"
+        links { "glfw", "GL", "dl", "pthread", "X11", "Xrandr", "Xi", "Xxf86vm", "m" }
+
+    filter "system:macosx"
+        links { "glfw", "glad" }
+        linkoptions { "-framework OpenGL" }
+
+    
+    
+   
+
+
+
+
+
