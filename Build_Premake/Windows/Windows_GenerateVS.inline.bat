6651d1cd8b3c9315f1e425fed9b18d26253fb1e7
Ion Filipski <armagedescu@gmail.com>
2025-10-10 11:35:32 +0300

    Add project Inline generator.

--- Build_Premake/Windows/Windows_GenerateVS.inline.bat
+++ Build_Premake/Windows/Windows_GenerateVS.inline.bat
@@ -3,10 +3,10 @@ setlocal ENABLEDELAYEDEXPANSION
 @REM the newly created folder is ignored by premake tool
 @REM mkdir "..\Project"
 @set resource_dir=..\..\resource\
-@set projects_dir=.\Project\
-@set application_dir=%projects_dir%\Application\
+@set projects_dir=..\..
+@set application_dir=%projects_dir%\application\
 mkdir %projects_dir%
-premake5.exe vs2022
+premake5.exe vs2022  --file=premake5.inline.lua
 xcopy /S /I %resource_dir%shader   %application_dir%shader
 xcopy /S /I %resource_dir%textures %application_dir%textures
 endlocal
---
+++ Build_Premake/Windows/premake5.inline.lua
@@ -0,0 +1,81 @@
+workspace "Zap-Project"
+    architecture "x64"
+
+    location "Project.Inline"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+	startproject "application"
+
+project "Zap"
+    location "Project.Inline/Zap"
+    kind "StaticLib"
+    language "C++"
+    files { "../../src/**.cpp"}
+    files { "../../src/**.h"}
+    files { "../../src/Graphics/**.cpp"}
+    files { "../../src/Graphics/**.h"}
+    files { "../../src/GUI**.cpp"}
+    files { "../../src/GUI/**.h"}
+    files { "../../src/Util/**.cpp"}
+    files { "../../src/Util/**.h"}
+    files { "../../src/Window/**.cpp"}
+    files { "../../src/Window/**.h"}
+    includedirs {"../../extern/"}
+    includedirs {"../../extern/glad/include/"}
+    includedirs {"../../extern/GLFW/include/"}
+    includedirs {"../../extern/freetype/include/Windows"}
+    cppdialect "C++17"
+
+    filter "configurations:Debug"
+        staticruntime "off"
+        runtime "Debug"
+
+
+project "application"
+    location "../../application"
+    kind "ConsoleApp"
+    language "C++"
+    files {"../../application/enabler.h"}
+    files {"../../application/main.cpp"}
+    files {"../../application/main_*.cpp"}
+    files {"../../application/Cube.*"}
+    files {"../../application/LightCube.*"}
+    files {"../../extern/glad.c"}
+    includedirs {"../../extern/glad/include/"}
+    includedirs {"../../extern/GLFW/include/"}
+    includedirs {"../../extern/freetype/include/Windows"}
+    includedirs {"../../src/"}
+    includedirs {"../../extern/"}
+    libdirs {"../../extern/GLFW/lib/Windows"}
+    libdirs {"../../extern/freetype/lib/Windows"}
+    libdirs {"../../Project/Zap2/bin/"}
+    links {"Zap"}
+
+    cppdialect "C++17"
+
+    filter "configurations:Debug"
+        staticruntime "off"
+        runtime "Debug"
+    
+    filter "system:windows"
+        links {"glfw3","opengl32","user32", "gdi32","shell32", "freetype"} -- Add other platforms support with freetype later
+    
+    filter "system:linux"
+        links { "glfw", "GL", "dl", "pthread", "X11", "Xrandr", "Xi", "Xxf86vm", "m" }
+
+    filter "system:macosx"
+        links { "glfw", "glad" }
+        linkoptions { "-framework OpenGL" }
+
+    
+    
+   
+
+
+
+
+
