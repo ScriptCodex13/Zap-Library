c5a034369c328abc79fdcb66e236b3dd59fe5bdd
Ion Filipski <armagedescu@gmail.com>
2025-10-16 12:55:28 +0300

    Add ButtonText class

---
+++ application/main_ui_texture_text_button.cpp
@@ -0,0 +1,97 @@
+#include "enabler.h"
+#ifdef SAMPLE_MAIN_UI_TEXTURE_TEXT_BUTTON_CPP
+
+// Just a example
+// PR = Prototyping -> only for testing 
+#include <Zap.h>
+#include <iostream>
+#include <array>
+
+
+template <typename T> class window_invoker : public zap::util::callback_invoker<T>
+{
+public:
+	inline window_invoker(T _callback) : callback_invoker(_callback) {}
+	template<typename A1> void operator () (A1& a1) { callback(a1); }
+};
+window_invoker  cbi([](zap::Window& window) {
+	if (window.isKeyPressed(zap::Key::ESC))
+	{
+		window.Close();
+	}
+});
+
+int main()
+{
+	//ZAP_DISABLE_OUTPUTS(true);
+
+	zap::Init(4, 6);
+
+	zap::Window window(1920, 1080, "Hello Window");
+	zap::util::scope_guard zapDeleter (zap::Delete);
+
+	zap::InitGlad();
+
+
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	// Window settings
+
+	window.UpdateViewport(true);
+	window.SetVSync(true);
+	zap::Enable(zap::Instructions::DEPTH);
+	zap::Enable(zap::Instructions::ANTIALIASING);
+
+
+	zap::ButtonText buttonText(window, std::array<float, 4> {-0.5, 0.6, 0.2, 0.8 }, "Button", "C:/Windows/Fonts/arial.ttf");
+
+	//
+	//buttonText.SetTextOffset(0.31f, 0.075f);
+	//buttonText.SetTextColor(zap::TextColors::RED);
+
+	////buttonText.GetTextObject()->SetTextOffset(0.31f, 0.075f);
+	////buttonText.GetTextObject()->SetTextColor(zap::TextColors::PURPLE);
+	////buttonText.GetTextObject()->GenerateCharacters();
+	////buttonText.SetGlPosition(std::array<float, 2> {- 0.9f, 0.7f});
+	////buttonText.SetGlPosition(-0.9f, 0.7f, 0.2, 0.8 );
+	////buttonText.SetGlSize(std::array<float, 2> {0.1f, 0.1f});
+	////buttonText.SetGlWidth(0.7);
+	////buttonText.SetGlHeight(0.28);
+	////buttonText.SetGlHeight(0.10);
+	//buttonText.SetColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	glDisable(GL_DEPTH_TEST);
+	while (window.Open())
+	{
+		//
+		cbi(window);
+		zap::ClearBuffers();
+		glClear(GL_COLOR_BUFFER_BIT);
+		zap::ClearBackground(0.2f, 0.3f, 0.3f, 1.0f);
+
+
+		buttonText.Bind();
+		if (buttonText.Hovered())
+			buttonText.SetColor(0.0f, 0.9f, 0.0f, 0.8f);
+		else
+			buttonText.SetColor(0.0f, 1.0f, 0.0f, 0.8f);
+
+		if (buttonText.Pressed(zap::Key::LEFT_MOUSE))
+			buttonText.SetColor(0.0f, 0.7f, 0.0f, 0.8f);
+
+		buttonText.printf(L"Button %f", glfwGetTime() * glfwGetTime() * glfwGetTime() * glfwGetTime());
+		buttonText.Draw();
+
+
+		window.Update();
+		window.Draw();
+	}
+
+	return 0;
+
+}
+
+
+#endif
--- src/GUI/Button.h
+++ src/GUI/Button.h
@@ -144,6 +144,120 @@ namespace zap
 		zap::Window* e_window;
 	};
 
+	class ButtonText : public zap::Mesh
+	{
+
+	public:
+		ButtonText(zap::Window& window, const std::string button_text = "", const std::string button_text_font_path = "");
+		ButtonText(zap::Window& window, const std::array<float, 4>& bounds, const std::string button_text = "", const std::string button_text_font_path = "");
+
+		~ButtonText();
+
+		int LoadTexture(unsigned int id, const std::string path_to_texture, zap::TextureFilter filter, int shader_location = 1);
+		void FinishMesh();
+		void UseTextureShaders(bool state);
+		void UseTextureShaders(const char* vertex_shader_source, const char* fragment_shader_source);
+
+		zap::Text* GetTextObject();
+		void UpdatePosition();
+		bool Hovered();
+
+		bool Pressed(zap::Key key); // Returns true if button is pressed and false if not
+		bool Released(zap::Key key);
+
+		void SetGlSize(float width, float height);
+		void SetGlSize(std::array<float, 2>& width_height);
+		void SetGlWidth(float width);
+		void SetGlHeight(float height);
+		void SetGlPosition(float xmin, float ymin); // Equivalent to move window to XY pos. Sets the xmin/ymin = bottom/left point to the given coordinates
+		void SetGlPosition(std::array<float, 2>& gl_xy_min); // Equivalent to move window to XY pos. Sets the top left point to the given coordinates
+		void SetGlPosition(float xmin, float ymin, float xmax, float ymax); // Fully sets position xmin/ymin=bottom/left  xmax/ymax=top/right
+		void SetGlPosition(std::array<float, 2>& gl_xy_min, std::array<float, 2>& gl_xy_max); // Fully sets position xmin/ymin=bottom/left  xmax/ymax=top/right
+		void SetGlPosition(std::array<float, 4>& gl_xy_min_xy_max); // Fully sets position xmin/ymin=bottom/left  xmax/ymax=top/right
+		void SetColor(float RED, float GREEN, float BLUE, float ALPHA);
+
+		void ActivateTexture(bool state);
+		void UseText(bool state);
+		void SetButtonText(const std::string text);
+		void SetTextOffset(float x_offset, float y_offset);
+		void SetTextColor(zap::TextColors color);
+		void SetTextColor(float RED, float GREEN, float BLUE);
+		void SetZCoordinate(); // Don't know if you need it
+
+		void Update();
+		void Draw(int texture_id = 0);
+
+
+
+		std::vector<wchar_t> wprintf_buffer;
+		int printf(const std::wstring content, ...)
+		{
+			//evaluate size first
+			va_list arglist;
+			va_start(arglist, content);
+			int result = _vsnwprintf(nullptr, 0, content.c_str(), arglist);
+			va_end(arglist);
+
+			zap::util::vector_realloc <wchar_t>(wprintf_buffer, result + 1);
+
+			//print text
+			va_start(arglist, content);
+			int retval = text.printf_t(this->GetTextureByHash(textureHash), wprintf_buffer.data(), result + 1, content.c_str(), arglist);
+			va_end(arglist);
+			return retval;
+		}
+
+	private: // Private functions
+
+	private:
+		zap::TextureText text;
+
+		glm::vec4 i_button_color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+		unsigned int textureHash;
+	protected:
+		unsigned int i_moveto_uniform_location;
+		unsigned int i_button_color_location;
+
+		std::array<float, 4> i_bounds; // x_min, x_max, y_min, y_max
+
+
+		const char* i_vertex_shader_source =
+			R"glsl(
+					#version 330 core
+					layout(location = 0) in vec3 aPos;
+					layout(location = 1) in vec2 aTexCoord;
+
+					uniform mat4 moveto;
+
+					out vec2 TexCoord;
+
+					void main()
+					{
+						gl_Position = moveto * vec4(aPos, 1.0);
+						TexCoord = aTexCoord;
+					}
+			)glsl";
+
+		const char* i_fragment_shader_source =
+			R"glsl(
+					#version 330 core
+					out vec4 FragColor;
+					in vec2 TexCoord;
+
+					uniform vec4 button_color;
+					uniform sampler2D ourTexture;
+
+					void main()
+					{
+                        vec4 texColor = texture(ourTexture, TexCoord);
+						FragColor = button_color + texColor;
+					}
+			)glsl";
+
+
+		zap::Window* e_window;
+	};
+
 }
 
 #endif
