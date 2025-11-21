454bd08cdef245d799bcde06bc0e5c8a7dfe3cae
Not Important <lukbauhome@gmail.com>
2025-11-21 20:32:42 +0100

    Added first audio test and set up headers

---
+++ application/main_audio_test.cpp
@@ -0,0 +1,13 @@
+#include "enabler.h"
+
+#ifdef SAMPLE_MAIN_AUDIO_TEST
+
+#include <iostream>
+#include <Audio.h>
+
+int main() 
+{
+	std::cout << "Hello World !" << std::endl;
+}
+
+#endif
---
+++ src/Audio/miniaudio.cpp
@@ -0,0 +1,4 @@
+/*/ miniaudio implementation /*/
+
+#define MINIAUDIO_IMPLEMENTATION
+#include <miniaudio.h>
