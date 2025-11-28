7f1b75d19b27a9fadc7c1573671247d5f5c7bd8b
Not Important <lukbauhome@gmail.com>
2025-11-28 22:21:32 +0100

    In between save for Audio Device

--- zap_lib/Audio/Device.cpp
+++ zap_lib/Audio/Device.cpp
@@ -4,7 +4,7 @@ namespace zap
 {
 	AudioDevice::AudioDevice()
 	{
-		
+		i_device_config = ma_device_config();
 	}
 
 	AudioDevice::~AudioDevice()
@@ -12,5 +12,41 @@ namespace zap
 
 	}
 
+
+	void AudioDevice::SetPlaybackType(PlaybackTypes playbacktype)
+	{
+		data.i_playback_type = playbacktype;
+	}
+
+	void AudioDevice::SetFormat(PlaybackFormat playbackformat)
+	{
+		data.i_playback_format = playbackformat;
+	}
+
+	void AudioDevice::SetChannels(unsigned int channels)
+	{
+		data.i_channels = channels;
+	}
+
+	void AudioDevice::SetSampleRate(unsigned int samplerate)
+	{
+		data.i_sample_rate = samplerate;
+	}
+
+	void AudioDevice::SetCallback() 
+	{
+
+	} 
+	
+	// Dont know if it works
+
+	void AudioDevice::Finish()
+	{
+		i_device_config = ma_device_config_init((ma_device_type)data.i_playback_type);
+		i_device_config.playback.format = (ma_format)data.i_playback_format;
+		i_device_config.playback.channels = data.i_channels;
+		i_device_config.sampleRate = data.i_sample_rate;
+		i_device_config.dataCallback = i_datacallback;
+	}
 
 }
--- zap_lib/Audio/Device.h
+++ zap_lib/Audio/Device.h
@@ -7,16 +7,56 @@
 
 namespace zap
 {
-	class AudioDevice : private ma_device_config
+	enum class PlaybackTypes
+	{
+		DEFAULT = 0,
+		PLAYBACK = ma_device_type_playback,
+		CAPTURE = ma_device_type_capture,
+		DUPLEX = ma_device_type_duplex,
+		LOOPBACK = ma_device_type_loopback
+	};
+
+	enum class PlaybackFormat
+	{
+		AUTO = ma_format_unknown,
+		F32 = ma_format_f32,
+		SI16 = ma_format_s16,
+		SI24 = ma_format_s24,
+		SI32 = ma_format_s32,
+		UI8 = ma_format_u8
+	};
+
+	struct DeviceConfigData
+	{
+		PlaybackTypes i_playback_type = PlaybackTypes::DEFAULT;
+		PlaybackFormat i_playback_format = PlaybackFormat::AUTO;
+		unsigned int i_channels = 1;
+		unsigned int i_sample_rate = 48000;
+
+	};
+
+	class AudioDevice
 	{
 	public:
 		AudioDevice();
 		~AudioDevice();
 
+		void SetPlaybackType(PlaybackTypes playbacktype);
+		void SetFormat(PlaybackFormat playbackformat);
+		void SetChannels(unsigned int channels);
+		void SetSampleRate(unsigned int samplerate);
+		void SetCallback(); // Dont know if it works
+
 		void Finish();
 
+		void i_datacallback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) 
+		{
+
+		};
+
 	private:
-		
+		ma_device_config i_device_config;
+		DeviceConfigData data = DeviceConfigData();
 	};
 }
 
