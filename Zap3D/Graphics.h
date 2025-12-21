5b6534148fdcf689786905140a3cc8dece773b9b
Not Important <lukbauhome@gmail.com>
2025-12-21 12:47:45 +0100

    Added assimp static libs

--- Zap3D/Graphics.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "Mesh.h"
-#include "SceneCamera.h"
--- zap_lib/Audio/miniaudio.cpp
+++
@@ -1,7 +0,0 @@
-/*/ miniaudio implementation /*/
-
-#ifndef MINIAUDIO_IMPLEMENTATION
-	#define MINIAUDIO_IMPLEMENTATION
-#endif
-
-#include <miniaudio.h>
