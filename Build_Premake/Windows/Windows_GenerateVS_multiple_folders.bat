785d98ef7277b5b7ee2514e5414f8a2831422803
Not Important <lukbauhome@gmail.com>
2025-12-23 19:53:43 +0100

    Additions for model loading

--- Build_Premake/Windows/Windows_GenerateVS_multiple_folders.bat
+++ Build_Premake/Windows/Windows_GenerateVS_multiple_folders.bat
@@ -10,5 +10,6 @@ premake5.exe vs2022  --file=premake5_multiple_folders.lua
 xcopy /S /I %resource_dir%shader   %application_dir%shader
 xcopy /S /I %resource_dir%textures %application_dir%textures
 xcopy /S /I %resource_dir%sounds   %application_dir%sounds
+xcopy /S /I %resource_dir%models   %application_dir%models
 endlocal
 PAUSE
--- application/main_assimp_model_loading.cpp
+++ application/main_assimp_model_loading.cpp
@@ -5,6 +5,20 @@
 #include <Zap.h>
 #include <Graphics.h>
 
+std::string vertex_shader_source = 
+	R"glsl(
+			#version 330 core
+
+			layout(location = 0) in vec3 aPos;
+
+			uniform mat4 model;
+
+			void main()
+			{
+				gl_Position = model * vec4(aPos, 1.0);
+			}
+	)glsl";
+
 int main()
 {
 	zap::Init(4, 6);
@@ -13,13 +27,35 @@ int main()
 
 	zap::InitGlad();
 
-	zap::ModelData modeldata = zap::LoadModel("Untitled.obj");
+	zap::ModelData modeldata = zap::LoadModel("models/TestCube.obj");
 
 	window.Maximize();
 	window.UpdateViewport(true);
 
+	for(auto i : modeldata.meshes)
+	{
+		for(auto s : i.vertices)
+		{
+			std::cout << s << std::endl;
+		}
+	}
+
+	zap::Mesh mesh(modeldata.meshes[0].vertices, modeldata.meshes[0].indices);
+
+	mesh.SetAttribPointer(0, 3, 3, 0);
+
+	mesh.Finish();
+
+
+
 	while(window.Open())
 	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		zap::ShowWireFrame(window.isKeyPressed(zap::Key::F10));
+
+		mesh.Bind();
+		mesh.Draw();
 
 		window.Update();
 		window.Draw();
